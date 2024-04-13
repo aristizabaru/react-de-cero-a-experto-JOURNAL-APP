92dc110b485b3c538a2e0c25df865117ae54a2cf
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-13 10:11:52 -0500

    feat (AppRouter.jsx): boiler plate de functional component de router
    Se establece estructura para rutas en módulos auth y journal

---
+++ src/modules/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Router, Route } from 'react-router-dom';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+
+            {/* Login y registro */}
+            <Route />
+
+            {/* Journal App */}
+            <Route />
+
+        </Router>
+    );
+};
