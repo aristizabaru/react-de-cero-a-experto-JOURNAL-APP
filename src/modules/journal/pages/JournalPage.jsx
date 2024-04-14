f4bf58664a5ddd6b728f608796b9a3c8e8c62e86
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-14 06:49:42 -0500

    feat (JournalPage.jsx): se implementa un texto para su visualización inicial
    Se hace a modo de test

--- src/modules/journal/pages/JournalPage.jsx
+++ src/modules/journal/pages/JournalPage.jsx
@@ -1,5 +1,10 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from "../layout/JournalLayout";
+
 export const JournalPage = () => {
     return (
-        <div>JournalPage</div>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, odio consequatur nesciunt cumque vero enim earum labore voluptatem pariatur sint dicta obcaecati eveniet illo atque aut reprehenderit libero repudiandae doloribus.</Typography>
+        </JournalLayout>
     );
 };
