2c74a57302ebf46af13929d21bf17ca6fe1379cb
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-14 06:47:13 -0500

    feat (RegisterPage.jsx): implementación del UI de página de registro

--- src/modules/auth/pages/RegisterPage.jsx
+++ src/modules/auth/pages/RegisterPage.jsx
@@ -1,5 +1,58 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <AuthLayout title="Crear cuenta">
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                        <TextField
+                            label="Nombre completo"
+                            type="text"
+                            placeholder="Nombre completo"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@google.com"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                        <TextField
+                            label="Contraseña"
+                            type="password"
+                            placeholder="contraseña"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid
+                        container
+                        spacing={2}
+                        sx={{ marginBottom: 2, marginTop: 1 }}
+                    >
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth>Crear cuenta</Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container direction="row" justifyContent="end">
+                        <Typography sx={{ marginRight: 1 }}>¿Ya tienes cuenta?</Typography>
+                        <Link
+                            component={RouterLink}
+                            color="inherit"
+                            to="/auth/login"
+                        >
+                            Ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };
