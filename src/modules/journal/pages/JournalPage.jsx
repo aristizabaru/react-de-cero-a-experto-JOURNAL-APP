52609a93c5b91c60cffc5ec91443f6dca7b02355
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-14 09:36:59 -0500

    feat (JournalPage.jsx): adición de componente de botón en UI para generación de nuevas entradas

--- src/modules/journal/pages/JournalPage.jsx
+++ src/modules/journal/pages/JournalPage.jsx
@@ -1,10 +1,25 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from '../views';
+import { NoteView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <NothingSelectedView />
+            <NoteView />
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     );
 };
