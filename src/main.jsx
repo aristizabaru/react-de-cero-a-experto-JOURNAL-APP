2b6b19fedafcc830c9c0913d0a5fd413d7172f6b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-20 05:59:27 -0500

    style: se agrega espacio entre llaves

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={ store }>
       <BrowserRouter>
         <JournalApp />
       </BrowserRouter>
