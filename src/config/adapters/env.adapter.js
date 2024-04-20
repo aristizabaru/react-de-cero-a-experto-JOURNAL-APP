0265b4a69469c576898c865dc1e28531c7560113
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-20 06:06:20 -0500

    fix (env.adapter.js): se corrige configuración de importación de variables de entorno

--- src/config/adapters/env.adapter.js
+++ src/config/adapters/env.adapter.js
@@ -1,11 +1,19 @@
-import 'dotenv/config';
-import { get } from 'env-var';
+import { from } from 'env-var';
+const { get } = from({
+    API_KEY: import.meta.env.VITE_API_KEY,
+    AUTH_DOMAIN: import.meta.env.VITE_AUTH_DOMAIN,
+    PROJECT_ID: import.meta.env.VITE_PROJECT_ID,
+    STORAGE_BUCKET: import.meta.env.VITE_STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    APP_ID: import.meta.env.VITE_APP_ID,
+});
 
 export const envs = {
     API_KEY: get('API_KEY').required().asString(),
     AUTH_DOMAIN: get('AUTH_DOMAIN').required().asString(),
     PROJECT_ID: get('PROJECT_ID').required().asString(),
     STORAGE_BUCKET: get('STORAGE_BUCKET').required().asString(),
-    MESSAGING_SENDER_ID: get('MESSAGING_SENDER_ID').required().asString(),
+    MESSAGING_SENDER_ID: get('MESSAGING_SENDER_ID').required().asInt(),
     APP_ID: get('APP_ID').required().asString(),
 };
+
