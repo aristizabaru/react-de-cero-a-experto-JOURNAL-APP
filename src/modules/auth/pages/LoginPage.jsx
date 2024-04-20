5b1582ae9ffaf9b22066f67fc5696c2fda7d6d59
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-20 04:24:43 -0500

    feat (LoginPage.jsx): se implementan event handlers para los submit del formulario

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -1,12 +1,35 @@
+import { useDispatch } from "react-redux";
 import { Link as RouterLink } from "react-router-dom";
 import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 import { Google } from "@mui/icons-material";
+
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../shared/hooks/useForm";
+import { checkingAuthentication, startGoogleSingIn } from "../../../store/auth";
 
 export const LoginPage = () => {
+
+    const dispatch = useDispatch();
+
+    const { email, password, onInputChange } = useForm({
+        email: 'andres.aristizabal@gmail.com',
+        password: '123456',
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(checkingAuthentication({ email, password }));
+        console.log({ email, password });
+    };
+
+    const onGoogleSingIn = () => {
+        dispatch(startGoogleSingIn());
+    };
+
     return (
         <AuthLayout title="Login">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ marginTop: 2 }}>
                         <TextField
@@ -14,6 +37,9 @@ export const LoginPage = () => {
                             type="email"
                             placeholder="correo@google.com"
                             fullWidth
+                            name='email'
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ marginTop: 2 }}>
@@ -22,6 +48,9 @@ export const LoginPage = () => {
                             type="password"
                             placeholder="contraseña"
                             fullWidth
+                            name='password'
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid
@@ -30,10 +59,10 @@ export const LoginPage = () => {
                         sx={{ marginBottom: 2, marginTop: 1 }}
                     >
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>Login</Button>
+                            <Button type="submit" variant="contained" fullWidth>Login</Button>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>
+                            <Button onClick={onGoogleSingIn} variant="contained" fullWidth>
                                 <Google />
                                 <Typography sx={{ marginLeft: 1 }}>Google</Typography>
                             </Button>
