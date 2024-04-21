cff3aacb7ea287e6f20b1291e5597cc72bb947ad
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-21 17:44:06 -0500

    style (thunks.js) se habilitan espacios entre paréntesis y corchetes para mejorar la legibilidad

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,13 +1,19 @@
-import { checkingCredentials } from "./authSlice";
+import { singInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, logout, login } from "./authSlice";
 
-export const checkingAuthentication = ({ email, password }) => {
-    return async (dispatch) => {
-        dispatch(checkingCredentials());
+export const checkingAuthentication = ( { email, password } ) => {
+    return async ( dispatch ) => {
+        dispatch( checkingCredentials() );
     };
 };
 
 export const startGoogleSingIn = () => {
-    return async (dispatch) => {
-        dispatch(checkingCredentials());
+    return async ( dispatch ) => {
+        dispatch( checkingCredentials() );
+        const result = await singInWithGoogle();
+
+        if ( !result.ok ) return dispatch( logout( result.errorMessage ) );
+
+        dispatch( login( result ) );
     };
 };
