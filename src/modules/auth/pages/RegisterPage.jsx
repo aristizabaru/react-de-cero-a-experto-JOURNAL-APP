9cdd0c950c9d302afd02881622862790c642257e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-21 20:01:04 -0500

    feat (RegisterPage.jsx): se implementa validación de campos para determinar envío de formulario
    La validación de los campos se hace a nivel del custom hook useForm al que se el comparte un objeto con los parámetros de validación

--- src/modules/auth/pages/RegisterPage.jsx
+++ src/modules/auth/pages/RegisterPage.jsx
@@ -1,50 +1,99 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Grid, Typography, TextField, Button, Link } from "@mui/material";
-import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from '../../shared/hooks';
+import { useState } from 'react';
+
+const formData = {
+    displayName: '',
+    email: '',
+    password: '',
+};
+
+const formValidations = {
+    email: [ ( value ) => value.includes( '@' ), 'El correo debe tener una @' ],
+    password: [ ( value ) => value.length >= 6, 'El password debe tener más de 6 letras' ],
+    displayName: [ ( value ) => value.length >= 1, 'El nombre es obligatorio' ],
+};
 
 export const RegisterPage = () => {
+
+    const [ formSubmitted, setFormSubmitted ] = useState( false );
+
+    const {
+        formState, displayName, email, password,
+        formValid, displayNameValid, emailValid, passwordValid,
+        onInputChange,
+    } = useForm( formData, formValidations );
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        setFormSubmitted( true );
+        console.log( formState );
+    };
+
     return (
         <AuthLayout title="Crear cuenta">
-            <form>
+            <form onSubmit={ onSubmit }>
                 <Grid container>
-                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <Grid item xs={ 12 } sx={ { marginTop: 2 } }>
                         <TextField
                             label="Nombre completo"
                             type="text"
                             placeholder="Nombre completo"
                             fullWidth
+                            name='displayName'
+                            value={ displayName }
+                            onChange={ onInputChange }
+                            error={ !!displayNameValid && formSubmitted }
+                            helperText={ displayNameValid }
                         />
                     </Grid>
-                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <Grid item xs={ 12 } sx={ { marginTop: 2 } }>
                         <TextField
                             label="Correo"
                             type="email"
                             placeholder="correo@google.com"
                             fullWidth
+                            name='email'
+                            value={ email }
+                            onChange={ onInputChange }
+                            error={ !!emailValid && formSubmitted }
+                            helperText={ emailValid }
                         />
                     </Grid>
-                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <Grid item xs={ 12 } sx={ { marginTop: 2 } }>
                         <TextField
                             label="Contraseña"
                             type="password"
                             placeholder="contraseña"
                             fullWidth
+                            name='password'
+                            value={ password }
+                            onChange={ onInputChange }
+                            error={ !!passwordValid && formSubmitted }
+                            helperText={ passwordValid }
                         />
                     </Grid>
                     <Grid
                         container
-                        spacing={2}
-                        sx={{ marginBottom: 2, marginTop: 1 }}
+                        spacing={ 2 }
+                        sx={ { marginBottom: 2, marginTop: 1 } }
                     >
-                        <Grid item xs={12}>
-                            <Button variant="contained" fullWidth>Crear cuenta</Button>
+                        <Grid item xs={ 12 }>
+                            <Button
+                                type='submit'
+                                variant="contained"
+                                fullWidth
+                            >
+                                Crear cuenta
+                            </Button>
                         </Grid>
                     </Grid>
                     <Grid container direction="row" justifyContent="end">
-                        <Typography sx={{ marginRight: 1 }}>¿Ya tienes cuenta?</Typography>
+                        <Typography sx={ { marginRight: 1 } }>¿Ya tienes cuenta?</Typography>
                         <Link
-                            component={RouterLink}
+                            component={ RouterLink }
                             color="inherit"
                             to="/auth/login"
                         >
