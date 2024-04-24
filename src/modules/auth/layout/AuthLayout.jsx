1f3e5742b6235bdecd98041e4fc0ab3e33a1c95d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:14:47 -0500

    style: se adicionan espacio entre llaves y corchetes para mejorar legibilidad

--- src/modules/auth/layout/AuthLayout.jsx
+++ src/modules/auth/layout/AuthLayout.jsx
@@ -1,28 +1,30 @@
 import { Grid, Typography } from "@mui/material";
 
-export const AuthLayout = ({ children, title = '' }) => {
+// eslint-disable-next-line react/prop-types
+export const AuthLayout = ( { children, title = '' } ) => {
     return (
         <Grid
+
             container
-            spacing={0}
+            spacing={ 0 }
             direction="column"
             alignItems="center"
             justifyContent="center"
-            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+            sx={ { minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 } }
         >
             <Grid
                 item
                 className="box-shadow"
-                xs={3}
-                sx={{
+                xs={ 3 }
+                sx={ {
                     width: { xs: 450 },
                     backgroundColor: 'white',
                     padding: 3,
                     borderRadius: 2
-                }}
+                } }
             >
-                <Typography variant="h5" sx={{ marginBottom: 5 }}>{title}</Typography>
-                {children}
+                <Typography variant="h5" sx={ { marginBottom: 5 } }>{ title }</Typography>
+                { children }
             </Grid>
         </Grid>
     );
