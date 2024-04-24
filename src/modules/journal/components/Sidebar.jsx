2f137600df042147b4fa1ef5db87a76e23ab71ef
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:19:16 -0500

    style: se adicionan espacio entre llaves y corchetes para mejorar legibilidad

--- src/modules/journal/components/Sidebar.jsx
+++ src/modules/journal/components/Sidebar.jsx
@@ -3,43 +3,47 @@ import {
     ListItemIcon, ListItemText, Toolbar, Typography
 } from '@mui/material';
 import { TurnedInNot } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
-export const Sidebar = ({ drawerWidth = 240 }) => {
+export const Sidebar = ( { drawerWidth = 240 } ) => {
+
+    const { displayName } = useSelector( state => state.auth );
+
     return (
         <Box
             component='nav'
-            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            sx={ { width: { sm: drawerWidth }, flexShrink: { sm: 0 } } }
         >
             <Drawer
                 variant='permanent' // temporary
                 open
-                sx={{
+                sx={ {
                     display: { xs: 'block' },
                     '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
-                }}
+                } }
             >
                 <Toolbar>
                     <Typography variant='h6' noWrap component='div'>
-                        Fernando Herrera
+                        { displayName }
                     </Typography>
                 </Toolbar>
                 <Divider />
 
                 <List>
                     {
-                        ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
-                            <ListItem key={text} disablePadding>
+                        [ 'Enero', 'Febrero', 'Marzo', 'Abril' ].map( text => (
+                            <ListItem key={ text } disablePadding>
                                 <ListItemButton>
                                     <ListItemIcon>
                                         <TurnedInNot />
                                     </ListItemIcon>
                                     <Grid container>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={'Exercitation cillum irure elit consectetur.'} />
+                                        <ListItemText primary={ text } />
+                                        <ListItemText secondary={ 'Exercitation cillum irure elit consectetur.' } />
                                     </Grid>
                                 </ListItemButton>
                             </ListItem>
-                        ))
+                        ) )
                     }
                 </List>
 
