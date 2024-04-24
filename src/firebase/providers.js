fce121d1a9d061b6a89f6970203aef2cbb857855
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:14:03 -0500

    feat (providers.js): se implementan los providers para el manejo de la autenticación

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,14 +1,14 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
 
 export const singInWithGoogle = async () => {
     try {
-        const result = await signInWithPopup( FirebaseAuth, googleProvider );
+        const response = await signInWithPopup( FirebaseAuth, googleProvider );
         // Obtiene el token para verificar del lado de Google
-        // const credentials = GoogleAuthProvider.credentialFromResult( result );
-        const { displayName, email, photoURL, uid } = result.user;
+        // const credentials = GoogleAuthProvider.credentialFromResult( response );
+        const { displayName, email, photoURL, uid } = response.user;
 
         return {
             ok: true,
@@ -28,4 +28,60 @@ export const singInWithGoogle = async () => {
             errorMessage,
         };
     }
+};
+
+export const loginWithEmailPassword = async ( { email, password } ) => {
+    try {
+        const response = await signInWithEmailAndPassword( FirebaseAuth, email, password );
+        const { displayName, photoURL, uid } = response.user;
+
+        return {
+            ok: true,
+            displayName,
+            photoURL,
+            uid,
+            email,
+        };
+
+    } catch ( error ) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        return {
+            ok: false,
+            errorCode,
+            errorMessage,
+        };
+    }
+};
+
+export const registerUserWithEmailPassword = async ( { email, password, displayName } ) => {
+    try {
+        const response = await createUserWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL } = response.user;
+
+        await updateProfile( FirebaseAuth.currentUser, { displayName } );
+
+        return {
+            ok: true,
+            uid,
+            photoURL,
+            email,
+            displayName,
+        };
+
+    } catch ( error ) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        return {
+            ok: false,
+            errorCode,
+            errorMessage,
+        };
+    }
+};
+
+export const logoutFirebase = async () => {
+    return await FirebaseAuth.signOut();
 };
