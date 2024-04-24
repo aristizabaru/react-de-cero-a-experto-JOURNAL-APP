21ef13677a2ea7a31a3dc92392fedec893f0788c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:24:35 -0500

    feat (AppRouter.jsx): se implementa protección de rutas basado en estado de la autenticación

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { AuthRoutes } from '../modules/auth/routes/AuthRoutes';
 import { JournalRoutes } from '../modules/journal/routes/JournalRoutes';
 
+import { CheckingAuth } from '../modules/shared/ui';
+import { useCheckAuth } from '../modules/shared/hooks';
+
 export const AppRouter = () => {
+
+    const { status } = useCheckAuth();
+
+    if ( status === 'checking' ) {
+        return <CheckingAuth />;
+    }
+
     return (
         <Routes>
 
-            {/* Login y registro */}
-            <Route path='auth/*' element={<AuthRoutes />} />
+            { ( status === 'authenticated' )
+                ? <Route path='*' element={ <JournalRoutes /> } />
+                : <Route path='auth/*' element={ <AuthRoutes /> } />
+            }
 
-            {/* Journal App */}
-            <Route path='*' element={<JournalRoutes />} />
+            <Route path='/*' element={ <Navigate to='/auth/login' /> } />
+
+            {/* Login y registro */ }
+            {/* <Route path='auth/*' element={ <AuthRoutes /> } /> */ }
+
+            {/* Journal App */ }
+            {/* <Route path='*' element={ <JournalRoutes /> } /> */ }
 
         </Routes>
     );
