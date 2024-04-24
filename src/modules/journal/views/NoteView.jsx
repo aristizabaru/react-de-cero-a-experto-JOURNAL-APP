07ec9cc34717e59ef98a68460ec51b32d83ff742
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:19:40 -0500

    style: se adicionan espacio entre llaves y corchetes para mejorar legibilidad

--- src/modules/journal/views/NoteView.jsx
+++ src/modules/journal/views/NoteView.jsx
@@ -4,24 +4,30 @@ import { ImageGallery } from "../components";
 
 export const NoteView = () => {
     return (
-        <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: 1 }}>
+        <Grid
+            className='animate__animated animate__fadeIn animate__faster'
+            container direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={ { mb: 1 } }
+        >
             <Grid item>
-                <Typography fontSize={39} fontWeight="light">28 de Mayo, 2024</Typography>
+                <Typography fontSize={ 39 } fontWeight="light">28 de Mayo, 2024</Typography>
             </Grid>
             <Grid item>
-                <Button color="primary" sx={{ padding: 2 }}>
-                    <SaveOutlined sx={{ fontSize: 20, mr: 1 }} />
+                <Button color="primary" sx={ { padding: 2 } }>
+                    <SaveOutlined sx={ { fontSize: 20, mr: 1 } } />
                     Guardar
                 </Button>
             </Grid>
-            <Grid container sx={{ mt: 2 }}>
+            <Grid container sx={ { mt: 2 } }>
                 <TextField
                     type="text"
                     variant="filled"
                     fullWidth
                     placeholder="Ingrese un título"
                     label="Título"
-                    sx={{ border: 'none', mb: 1 }}
+                    sx={ { border: 'none', mb: 1 } }
                 />
                 <TextField
                     type="text"
@@ -29,7 +35,7 @@ export const NoteView = () => {
                     fullWidth
                     multiline
                     placeholder="¿Qué sucedió en el día de hoy?"
-                    minRows={5}
+                    minRows={ 5 }
                 />
             </Grid>
             <ImageGallery />
