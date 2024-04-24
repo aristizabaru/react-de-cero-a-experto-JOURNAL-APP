74a459b83eeff1055e4f2957a985507225e999f0
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:19:09 -0500

    feat (Navbar.jsx): se implementa acción del botón de logout para cerrar sesión de usuario

--- src/modules/journal/components/Navbar.jsx
+++ src/modules/journal/components/Navbar.jsx
@@ -1,20 +1,31 @@
+import { useDispatch } from 'react-redux';
 import { AppBar, Toolbar, IconButton, Grid, Typography } from "@mui/material";
 import { LogoutOutlined, MenuOutlined } from "@mui/icons-material";
+import { startLogout } from '../../../store/auth/thunks';
 
-export const Navbar = ({ drawerWidth = 240 }) => {
+
+// eslint-disable-next-line react/prop-types
+export const Navbar = ( { drawerWidth = 240 } ) => {
+
+    const dispatch = useDispatch();
+
+    const onLogout = () => {
+        dispatch( startLogout() );
+    };
+
     return (
         <AppBar
             position="fixed"
-            sx={{
+            sx={ {
                 width: { sm: `calc(100% - ${drawerWidth}px)` },
                 ml: { sm: `${drawerWidth}px` }
-            }}
+            } }
         >
             <Toolbar>
                 <IconButton
                     color="inherit"
                     edge="start"
-                    sx={{ mr: 2, display: { sm: 'none', } }}
+                    sx={ { mr: 2, display: { sm: 'none', } } }
                 >
                     <MenuOutlined />
                 </IconButton>
@@ -25,7 +36,10 @@ export const Navbar = ({ drawerWidth = 240 }) => {
                     alignItems="center"
                 >
                     <Typography variant="h6" noWrap component="div">Journal App</Typography>
-                    <IconButton color="error">
+                    <IconButton
+                        color="error"
+                        onClick={ onLogout }
+                    >
                         <LogoutOutlined />
                     </IconButton>
                 </Grid>
