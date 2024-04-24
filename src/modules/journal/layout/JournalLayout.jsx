29caecd69e6730cc1c1447da4d393601589c9488
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:19:25 -0500

    style: se adicionan espacio entre llaves y corchetes para mejorar legibilidad

--- src/modules/journal/layout/JournalLayout.jsx
+++ src/modules/journal/layout/JournalLayout.jsx
@@ -3,17 +3,21 @@ import { Navbar, Sidebar } from '../components';
 
 const drawerWidth = 240;
 
-export const JournalLayout = ({ children }) => {
+// eslint-disable-next-line react/prop-types
+export const JournalLayout = ( { children } ) => {
     return (
-        <Box sx={{ display: 'flex' }}>
-            <Navbar drawerWidth={drawerWidth} />
-            <Sidebar drawerWidth={drawerWidth} />
+        <Box
+            sx={ { display: 'flex' } }
+            className='animate__animated animate__fadeIn animate__faster'
+        >
+            <Navbar drawerWidth={ drawerWidth } />
+            <Sidebar drawerWidth={ drawerWidth } />
             <Box
                 component='main'
-                sx={{ flexGrow: 1, padding: 3 }}
+                sx={ { flexGrow: 1, padding: 3 } }
             >
                 <Toolbar />
-                {children}
+                { children }
             </Box>
         </Box>
     );
