9672a5e8a006fe7e9f669829eac811a45a601183
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:22:09 -0500

    feat (useCheckAuth.js): se implementa custom hook para validar estado de autenticación en la aplicación cuando se carga por primera vez

---
+++ src/modules/shared/hooks/useCheckAuth.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../../../firebase/config';
+import { login, logout } from '../../../store/auth';
+
+export const useCheckAuth = () => {
+    const { status } = useSelector( state => state.auth );
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        onAuthStateChanged( FirebaseAuth, async ( user ) => {
+
+            if ( !user ) return dispatch( logout() );
+
+            const { uid, photoURL, displayName, email } = user;
+            dispatch( login( { uid, photoURL, displayName, email } ) );
+
+        } );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [] );
+
+    return {
+        status,
+    };
+};
