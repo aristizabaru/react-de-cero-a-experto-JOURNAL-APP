180d9ea051534fb53de7ec6525230faef8ab64c0
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-23 20:19:32 -0500

    style: se adicionan espacio entre llaves y corchetes para mejorar legibilidad

--- src/modules/journal/pages/JournalPage.jsx
+++ src/modules/journal/pages/JournalPage.jsx
@@ -9,16 +9,16 @@ export const JournalPage = () => {
             <NoteView />
             <IconButton
                 size='large'
-                sx={{
+                sx={ {
                     color: 'white',
                     backgroundColor: 'error.main',
                     ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
                     position: 'fixed',
                     right: 50,
                     bottom: 50,
-                }}
+                } }
             >
-                <AddOutlined sx={{ fontSize: 30 }} />
+                <AddOutlined sx={ { fontSize: 30 } } />
             </IconButton>
         </JournalLayout>
     );
