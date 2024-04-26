25551f1f12ac749b92360780a653b2d77c56379c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-26 15:48:44 -0500

    feat (NoteView.jsx): se implementan datos dinámicos en el formulario extraídos de nota activa en el estado global

--- src/modules/journal/views/NoteView.jsx
+++ src/modules/journal/views/NoteView.jsx
@@ -1,8 +1,21 @@
 import { SaveOutlined } from "@mui/icons-material";
 import { Grid, Typography, Button, TextField } from "@mui/material";
 import { ImageGallery } from "../components";
+import { useForm } from '../../shared/hooks/useForm';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
 
 export const NoteView = () => {
+
+    const { active: note } = useSelector( state => state.journal );
+    const { body, title, date, onInputChange, formState } = useForm( note );
+    const dateString = useMemo( () => {
+        const currentNoteDate = new Date( date );
+
+        return currentNoteDate.toUTCString();
+
+    }, [ date ] );
+
     return (
         <Grid
             className='animate__animated animate__fadeIn animate__faster'
@@ -12,7 +25,7 @@ export const NoteView = () => {
             sx={ { mb: 1 } }
         >
             <Grid item>
-                <Typography fontSize={ 39 } fontWeight="light">28 de Mayo, 2024</Typography>
+                <Typography fontSize={ 39 } fontWeight="light">{ dateString }</Typography>
             </Grid>
             <Grid item>
                 <Button color="primary" sx={ { padding: 2 } }>
@@ -28,6 +41,9 @@ export const NoteView = () => {
                     placeholder="Ingrese un título"
                     label="Título"
                     sx={ { border: 'none', mb: 1 } }
+                    name='title'
+                    value={ title }
+                    onChange={ onInputChange }
                 />
                 <TextField
                     type="text"
@@ -36,6 +52,9 @@ export const NoteView = () => {
                     multiline
                     placeholder="¿Qué sucedió en el día de hoy?"
                     minRows={ 5 }
+                    name='body'
+                    value={ body }
+                    onChange={ onInputChange }
                 />
             </Grid>
             <ImageGallery />
