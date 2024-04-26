cda368c3c795f4b5666a450ff5340cc186918f9e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-26 15:45:43 -0500

    feat (Sidebar.jsx): se implementa map de notas cargadas del estado global

--- src/modules/journal/components/Sidebar.jsx
+++ src/modules/journal/components/Sidebar.jsx
@@ -1,13 +1,12 @@
-import {
-    Box, Divider, Drawer, Grid, List, ListItem, ListItemButton,
-    ListItemIcon, ListItemText, Toolbar, Typography
-} from '@mui/material';
-import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { SidebarItem } from './';
 
+// eslint-disable-next-line react/prop-types
 export const Sidebar = ( { drawerWidth = 240 } ) => {
 
     const { displayName } = useSelector( state => state.auth );
+    const { notes } = useSelector( state => state.journal );
 
     return (
         <Box
@@ -31,18 +30,8 @@ export const Sidebar = ( { drawerWidth = 240 } ) => {
 
                 <List>
                     {
-                        [ 'Enero', 'Febrero', 'Marzo', 'Abril' ].map( text => (
-                            <ListItem key={ text } disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={ text } />
-                                        <ListItemText secondary={ 'Exercitation cillum irure elit consectetur.' } />
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map( note => (
+                            <SidebarItem key={ note.id } { ...note } />
                         ) )
                     }
                 </List>
