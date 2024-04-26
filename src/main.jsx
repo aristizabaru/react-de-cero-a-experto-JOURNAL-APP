f5a6e1f3897c03e7a0661e4a6822bf8e12b4c2aa
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-26 14:46:04 -0500

    style: se implementa espacio entre corchetes y paréntesis para mejorar legibilidad

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import { store } from './store';
 import { JournalApp } from './JournalApp';
 import './style.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
   <React.StrictMode>
     <Provider store={ store }>
       <BrowserRouter>
         <JournalApp />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
