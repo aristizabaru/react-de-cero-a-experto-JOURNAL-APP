5834b914a8dc070ba7a1cc3a35dab03e6e73fb6e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-26 15:50:49 -0500

    feat (useCheckAuth.jsx): se implementa carga de notas en estado global al iniciar la aplicación

--- src/modules/shared/hooks/useCheckAuth.js
+++ src/modules/shared/hooks/useCheckAuth.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAuthStateChanged } from 'firebase/auth';
 import { FirebaseAuth } from '../../../firebase/config';
 import { login, logout } from '../../../store/auth';
+import { startLoadingNotes } from '../../../store/journal';
+
 
 export const useCheckAuth = () => {
     const { status } = useSelector( state => state.auth );
@@ -15,6 +17,7 @@ export const useCheckAuth = () => {
 
             const { uid, photoURL, displayName, email } = user;
             dispatch( login( { uid, photoURL, displayName, email } ) );
+            dispatch( startLoadingNotes() );
 
         } );
         // eslint-disable-next-line react-hooks/exhaustive-deps
