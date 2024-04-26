b625615c024c10bcc7e52c27faa7aeee331dc80d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-26 14:48:17 -0500

    feat (useForm.js): se agrega useEffect para gestionar carga inicial del formulario si cambia la dependencia

--- src/modules/shared/hooks/useForm.js
+++ src/modules/shared/hooks/useForm.js
@@ -10,6 +10,12 @@ export const useForm = ( initialForm = {}, formValidations = {} ) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [ formState ] );
 
+    useEffect( () => {
+        setFormState( initialForm );
+
+    }, [ initialForm ] );
+
+
     const isFormValid = useMemo( () => {
         for ( const formValue of Object.keys( formValidation ) ) {
             if ( formValidation[ formValue ] !== null ) return false;
