546879e295b65a5f4c665bbe0f16343031131f2c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-29 15:09:38 -0500

    feat (journalSlice.js): se implementa reducers para setSaving y noteUpdated para la actualización de notas en el estado una vez salvadas

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -18,15 +18,22 @@ export const journalSlice = createSlice( {
         },
         setActiveNote: ( state, action ) => {
             state.active = action.payload;
+            state.messageSaved = '';
         },
         setNotes: ( state, action ) => {
             state.notes = action.payload;
         },
         setSaving: ( state ) => {
-
+            state.isSaving = true;
+            state.messageSaved = '';
         },
-        updateNote: ( state, action ) => {
-
+        noteUpdated: ( state, action ) => {
+            state.isSaving = false;
+            state.notes = state.notes.map( note => ( note.id === action.payload.id )
+                ? action.payload
+                : note
+            );
+            state.messageSaved = `${action.payload.title}, actualizada correctamente`;
         },
         deleteNodeById: ( state, action ) => {
 
@@ -43,5 +50,5 @@ export const {
     setActiveNote,
     setNotes,
     setSaving,
-    updateNote,
+    noteUpdated,
 } = journalSlice.actions;
