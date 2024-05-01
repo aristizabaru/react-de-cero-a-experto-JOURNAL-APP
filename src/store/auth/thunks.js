9d24732d3ad6f0e82e1d6ea21d3ec8d31e436854
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-30 20:51:22 -0500

    feat (thunks.js): se implementa dispatch  para limpiar notas del estado en el logout de la app

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
 import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, singInWithGoogle } from '../../firebase/providers';
+import { clearNotesLogout } from '../journal/journalSlice';
 import { checkingCredentials, logout, login } from "./authSlice";
 
 export const checkingAuthentication = ( { email, password } ) => {
@@ -47,6 +48,7 @@ export const startLogout = () => {
     return async ( dispatch ) => {
         await logoutFirebase();
 
+        dispatch( clearNotesLogout() );
         dispatch( logout() );
     };
 };
